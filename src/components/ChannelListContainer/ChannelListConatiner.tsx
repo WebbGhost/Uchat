6874de89c080b78c9deb663aaf77bb84e057de41
WebbGhost <thewebghost23@gmail.com>
2023-03-26 11:06:01 +0500

    Initail commit

---
+++ src/components/ChannelListContainer/ChannelListConatiner.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const ChannelListConatiner = (props: Props) => {
+    return <div>ChannelListConatiner</div>;
+};
+
+export default ChannelListConatiner;
